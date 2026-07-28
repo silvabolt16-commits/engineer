054650e4404ce7eeaf3c43ecdcbadef2fe01f8f4
Fajri Awwaluddin <fajriawwaluddin01@gmail.com>
2026-07-28 12:42:13 +0700

    Update static config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://fajriawwaluddin.com',
   integrations: [sitemap()],
   output: 'static',
-  adapter: cloudflare(),
   vite: {
     plugins: [tailwindcss()],
   },
