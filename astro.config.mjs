a37648b4adae6228313769072257046985088c22
Fajri Awwaluddin <fajriawwaluddin01@gmail.com>
2026-08-04 12:55:48 +0700

    SSG to SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://fajriawwaluddin.com',
   integrations: [sitemap()],
-  output: 'static',
+  output: 'server',
+  adapter: cloudflare(),
   vite: {
     plugins: [tailwindcss()],
   },
